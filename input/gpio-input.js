0ca0ca75bc15e31534a091ce994191d5da51bd93
Maarten Groeneweg <maarten@groeneweg.eu>
2022-01-15 16:57:40 +0100

    use int for input

--- input/gpio-input.js
+++ input/gpio-input.js
@@ -12,7 +12,7 @@ class GpioInput {
             //todo check for error
             console.log('value is');
             console.log(value);
-            if (value === false) {
+            if (value === 1) {
                 this.eventEmitter.emit('shotStarted');
             }
             else {
