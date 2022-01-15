1bf6b4595a5721c76d27ec0b2ebde6cc69606245
Maarten Groeneweg <maarten@groeneweg.eu>
2022-01-15 17:02:54 +0100

    added damping

--- input/gpio-input.js
+++ input/gpio-input.js
@@ -6,12 +6,9 @@ class GpioInput {
     
       init() {
         const Gpio = require('onoff').Gpio;
-        const button = new Gpio(4, 'in', 'both');
+        const button = new Gpio(4, 'in', 'rising', {debounceTimeout: 10});
         
         button.watch((err, value) => {
-            //todo check for error
-            console.log('value is');
-            console.log(value);
             if (value === 0) {
                 this.eventEmitter.emit('shotStarted');
             }
