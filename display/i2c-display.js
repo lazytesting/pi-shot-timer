eacc4d93e07e9d069448da9047d0e7fc1e147023
Maarten Groeneweg <maarten@groeneweg.eu>
2022-01-16 11:10:23 +0100

    re-enabled display of timer

--- display/i2c-display.js
+++ display/i2c-display.js
@@ -3,7 +3,7 @@ class I2CDisplay {
 
     var i2c = require('i2c-bus');
     var OLED = require('oled-i2c-bus');
-    var font = require('oled-font-5x7');
+    this.font = require('oled-font-5x7');
 
     const i2cBus = i2c.openSync(1)
 
@@ -13,20 +13,15 @@ class I2CDisplay {
         address: 0x3C
     };
 
-    const firstoled = new OLED(i2cBus, opts);
+    this.firstoled = new OLED(i2cBus, opts);
     firstoled.clearDisplay()
-    firstoled.setCursor(1, 1);
-    firstoled.writeString(font, 2, 'go!', 1, true);
-    console.log("Yes");
-    }
+  }
   
-
-
   displayTimer(millis) {
     console.log(millis);
-    // const prettyTime = `${Math.round(millis / 1000)} s`; //todo 0 safe
-    // this.oled.setCursor(1, 1);
-    // this.oled.writeString(font, 2, prettyTime, 1, true);
+    const prettyTime = `${Math.round(millis / 1000)} s`; //todo 0 safe
+    this.oled.setCursor(1, 1);
+    this.oled.writeString(this.font, 2, prettyTime, 1, true);
   }
 
 }
