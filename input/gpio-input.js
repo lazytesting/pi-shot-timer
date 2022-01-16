bfa18bb37112d682c2f16d42a71cbd79c94e7dc0
Maarten Groeneweg <maarten@groeneweg.eu>
2022-01-16 11:19:52 +0100

    increase debounce timeout

--- input/gpio-input.js
+++ input/gpio-input.js
@@ -6,7 +6,7 @@ class GpioInput {
     
       init() {
         const Gpio = require('onoff').Gpio;
-        const button = new Gpio(4, 'in', 'both', {debounceTimeout: 10});
+        const button = new Gpio(4, 'in', 'both', {debounceTimeout: 100});
         
         button.watch((err, value) => {
  
