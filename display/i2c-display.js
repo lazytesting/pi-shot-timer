1d1befdab436ecb99eb60e371db96b22c84e45fc
Maarten Groeneweg <maarten@groeneweg.eu>
2022-01-16 11:06:01 +0100

    move test to i2c display

--- display/i2c-display.js
+++ display/i2c-display.js
@@ -1,25 +1,32 @@
 class I2CDisplay {
   constructor() {
-    this.font =  require('oled-font-5x7');
-    const i2c = require('i2c-bus'),
-      i2cBus = i2c.openSync(1),
-      oled = require('oled-i2c-bus');
-    
+
+    var i2c = require('i2c-bus');
+    var OLED = require('oled-i2c-bus');
+    var font = require('oled-font-5x7');
+
+    const i2cBus = i2c.openSync(1)
+
     const opts = {
-      width: 128,
-      height: 64,
-      address: 0x3D
+        width: 128,
+        height: 32,
+        address: 0x3C
     };
 
-    this.oled = new oled(i2cBus, opts);
+    const firstoled = new OLED(i2cBus, opts);
+    firstoled.clearDisplay()
+    firstoled.setCursor(1, 1);
+    firstoled.writeString(font, 2, 'go!', 1, true);
+    console.log("Yes");
     }
   
 
 
   displayTimer(millis) {
-    const prettyTime = `${Math.round(millis / 1000)} s`; //todo 0 safe
-    this.oled.setCursor(1, 1);
-    this.oled.writeString(font, 2, prettyTime, 1, true);
+    console.log(millis);
+    // const prettyTime = `${Math.round(millis / 1000)} s`; //todo 0 safe
+    // this.oled.setCursor(1, 1);
+    // this.oled.writeString(font, 2, prettyTime, 1, true);
   }
 
 }
